65e589c67f039fef7610501641b0d69d26734c99
Software Especial <software@ulsa.ulsa-noroeste.edu.mx>
2019-08-26 12:51:05 -0700

    Agregar archivos de proyecto.

---
+++ Beisbol/Equipo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beisbol
+{
+    class Equipo
+    {
+        public string Nombre { get; set; }
+        public string Ciudad { get; set; }
+        public string Entrenador { get; set; }
+        public List Jugadores { get; set; }
+
+        public Jugadores()
+        {
+        Nombre: "Sin nombre"
+            Numero: "Sin numero"
+        }
+
+    }
+}
---
+++ Beisbol/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beisbol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Equipo> equipos = new List<Equipo>();
+
+            Equipo equipo_1 =
+                new Equipo("Equipo 1");
+
+            Equipo equipo_2 =
+                new Equipo("Equipo 2");
+
+            equipos.Add(equipo_1);
+            equipos.Add(equipo_2);
+
+
+        }
+    }
+}
